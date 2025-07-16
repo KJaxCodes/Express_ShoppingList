0c68fb844b8e621fcb1d6797722dd8aab20af47f
Kelly J <[kelly4410@gmail.com]>
2025-07-16 10:45:27 -0600

    update POST to check for repeat items

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-//show JSON 
+// configure express to accept JSON 
 app.use(express.json());
 
 const items = require('./fakeDb');
@@ -10,7 +10,7 @@ const items = require('./fakeDb');
 //check the root
 
 app.get('/', (req, res) => {
-    return res.send("Shopping List");
+    return res.json("Shopping List");
 })
 
 // GET /items
@@ -24,17 +24,36 @@ app.get('/items', (req, res) => {
 // POST /items
 app.post('/items', (req, res) => {
     console.log("POST /items route");
+    console.log("Creating an item. Data: ", req.body);
+
+    //we get the item 'name' and item 'price'
     const { name, price } = req.body;
-    const newItem = req.body;
+
+    //create item object
+    const newItem = { name, price }
+
+    //prevent repeating item names, make them unique
+    for (const item of items) {
+        if (item.name.toLowerCase() === name.toLowerCase()) {
+            return res.status(400).json({
+                message: `Did not create. Item name ${name} already exists`
+            });
+        }
+    }
+
+    //push into items
     items.push(newItem);
+    console.log("Items updated", items);
     return res.json({ added: newItem });
+
 })
 
 // GET /items/:name
+// name could equal anything 'laptop', 'milk', etc.
 app.get('/items/:name', (req, res) => {
     console.log("GET /items/:name route");
     console.log(req.params.name);
-    // get the item name entered
+    // get the item name from the URL params
     const item = req.params.name;
     // check if the item is in the shopping list, make generic not on list message if not
 
